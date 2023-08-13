d1aa933a45efb18b9c02f325097c67aae53a58a1
coderkartikeya <kartikeyavats04@gmail.com>
2023-08-13 18:12:27 +0530

    first commit

---
+++ style.js
@@ -0,0 +1,19 @@
+
+// var i = 0;
+// var txt = 'Web developer!!!!'; /* The text */
+// var speed = 1000; /* The speed/duration of the effect in milliseconds */
+
+// function typeWriter() {
+//   if (i < txt.length) {
+//     document.getElementById("post").innerHTML += txt.charAt(i);
+//     i++;
+//     setTimeout(typeWriter, speed);
+//   }
+//   if(document.getElementById('post').innerHTML===txt){
+//     document.getElementById('post').innerHTML='';
+//     i=0;
+//   }
+// }
+// setInterval(()=>{
+//     typeWriter();
+// },3000)
